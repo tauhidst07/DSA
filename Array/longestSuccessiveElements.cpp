e5569a82a8c3cf275bd566090839d1601e9f4b2b
Md Tauhid Ansari <116505896+tauhidst07@users.noreply.github.com>
2024-08-15 14:23:53 +0530

    Create longestSuccessiveElements.cpp

---
+++ Array/longestSuccessiveElements.cpp
@@ -0,0 +1,19 @@
+int longestSuccessiveElements(vector<int>&a) {
+    // Write your code here. 
+    int longest=0; 
+     unordered_map<int,int> mp; 
+     for(int i=0;i<a.size();i++){
+         mp[a[i]]=i;
+     } 
+     for(int i=0;i<a.size();i++){
+         int count=0;   
+         int value=1;
+         while(mp.find(a[i]+value)!=mp.end()){
+             count++;  
+             value++;
+             
+         } 
+         longest=max(count,longest);
+     }
+    return longest+1;
+}
